8e943397b397aa804fb483d4b77f0e648e3905be
a2602619 <a2602619@gmail.com>
2021-08-20 16:36:30 +0800

    upload project

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  if (restaurants.length > 0) {
+    res.render('index', { restaurants: restaurants, keyword: keyword })
+  } else {
+    res.render('notfind', { restaurants: restaurants, keyword: keyword })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`listening on localhost:${port}`)
+})
